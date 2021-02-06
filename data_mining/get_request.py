dbfb94f1da1eff259526c2e0d7d1c417066aacf5
Attila Kovács <kkovati@gmail.com>
2021-02-06 15:36:40 +0100

    Unstage Spyder project files
    Unstage Spyder project files

--- data_mining/get_request.py
+++ data_mining/get_request.py
@@ -3,23 +3,23 @@ import requests
 
 def get_request(url):
     response = requests.get(url)
-    
+
     if response.status_code != 200:
         raise Exception(f'HTTP Response Status Code: {response.status_code}')
     data_json = requests.get(url).json()
-        
+
     return data_json
 
 
 if __name__ == "__main__":
-    
+
     json = get_request("http://echo.jsontest.com/key/value/one/two")
-    
+
     assert json['key'] == 'value'
     assert json['one'] == 'two'
-    
+
     print("Test1 OK")
-    
+
     try:
         get_request("http://google.com")
     except Exception as e:
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         print("Test2 OK")
     else:
         raise Exception("Test2 Failed")
-        
+
     try:
         get_request("http://notworking.notworking")
     except Exception as e:
